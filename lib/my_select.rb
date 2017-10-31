51cdf920f1ef60607d3aba0b5af4792e3ec64435
devinencalada <devinencalada@gmail.com>
2017-10-30 21:02:31 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+  index = 0
+  answer_arr = []
+  while index < collection.length do
+    answer_arr << collection[index] if yield(collection[index]) != false
+    index += 1
+  end
+  answer_arr
 end
